0acc73e7370f31143a96512434f7497ab048a586
dooominiin <dominique.torti@students.fhnw.ch>
2023-08-25 07:50:20 +0200

    sd card hot swap

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ import duengermischung as dm
 from ds1307 import DS1307
 from EC_PH_Control import EC_Regler, EC_Sensor
 
-#sdcard karten test    
-try:
-    sd = sdcard.SDCard(SPI(0, 40_000_000, sck=Pin(18), mosi=Pin(19), miso=Pin(16)), Pin(17))
-    os.mount(sd, '/sd')
-    print(os.listdir('/sd'))
-    file = open("/sd/sample.txt","w")
-    for i in range(20):
-        file.write("Sample text = %s\r\n" % i)
-    file.close() 
-    print("sd karte Test erfolgreich\n")
-except Exception as e:
-    print("sd karte Test fehlgeschlagen")
-    print(e)
+if 1:
+    #sdcard karten test    
+    try:
+        sd = sdcard.SDCard(SPI(0, 40_000_000, sck=Pin(18), mosi=Pin(19), miso=Pin(16)), Pin(17))
+        os.mount(sd, '/sd')
+        print(os.listdir('/sd'))
+        file = open("/sd/sample.txt","w")
+        for i in range(20):
+            file.write("Sample text = %s\r\n" % i)
+        file.close() 
+        print("sd karte Test erfolgreich\n")
+    except Exception as e:
+        print("sd karte Test fehlgeschlagen")
+        print(e)
   
 
 if 1:
     
     try:
-        #sd = sdcard.SDCard(SPI(0, 40_000_000, sck=Pin(18), mosi=Pin(19), miso=Pin(16)), Pin(17))
         #os.mount(sd, '/sd')
         print(os.listdir('/sd'))
     except Exception as e:
@@ -46,8 +46,17 @@ if 1:
             file = open(file_path, "a") 
             file.write(str(val) + "\n")
             file.close()
+            p = Pumpe.Pumpe(15)
+            p.shot(0.1)
+            time.sleep(0.5)
         except Exception as e:
             print(e)
+            try:
+                sd = sdcard.SDCard(SPI(0, 40_000_000, sck=Pin(18), mosi=Pin(19), miso=Pin(16)), Pin(17))
+                os.umount(sd)
+                os.mount(sd, '/sd')
+            except Exception as e:
+                print(e)
 
 if 0:
     
